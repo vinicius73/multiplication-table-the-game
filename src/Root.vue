0e167acbb76596134eba76ce8cbafe5c8c08b8bf
Vinicius Reis <luiz.vinicius73@gmail.com>
2021-02-24 18:44:56 -0300

    ➕ add bulma css

---
+++ src/Root.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">
+        Hello World
+      </h1>
+      <p class="subtitle">
+        My first website with <strong>Bulma</strong>!
+      </p>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Root'
+})
+</script>
+
+<style>
+
+</style>
