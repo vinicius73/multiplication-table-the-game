fae104075010cfbb574ca0854a2e9d2a9774f5c2
Vinicius Reis <luiz.vinicius73@gmail.com>
2021-02-26 17:02:03 -0300

    🚸 add version

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,11 +1,34 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  setup () {
+    return  {
+      version: import.meta.env.VITE_APP_VERSION
+    }
+  }
+})
+</script>
+
 <template>
   <footer class="footer">
     <div class="content has-text-centered">
       <p>
+        <a href="https://github.com/vinicius73/multiplication-table-the-game" target="_blank" class="tag is-warning">
+          v{{ version }}
+        </a>
+      </p>
+      <p>
         By <a href="https://vinicius73.dev">Vinicius Reis</a>. <br>
-        The source code is licensed
+        The <a href="https://github.com/vinicius73/multiplication-table-the-game" target="_blank">source code</a> is licensed
         <a href="https://opensource.org/licenses/GPL-3.0">GPL-3.0+</a>.
       </p>
     </div>
   </footer>
 </template>
+
+<style scoped>
+footer {
+  padding-top: 1em;
+  padding-bottom: 1.5em;
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import pkg from './package.json'
+
+process.env.VITE_APP_VERSION = pkg.version
 
 // https://vitejs.dev/config/
 export default defineConfig({
